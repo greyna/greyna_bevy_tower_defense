f22aef7f09c4f3c537d42eb04b77c1383c97c0e3
Arthur VAN CEULEN <vandekeu@gmail.com>
2023-04-02 13:51:52 +0200

    Game and Menu states with related scheduling

--- src/game/schedule.rs
+++ src/game/schedule.rs
@@ -1,13 +1,27 @@
+use crate::AppState;
 use bevy::prelude::*;
 
 pub struct SchedulePlugin;
 
 impl Plugin for SchedulePlugin {
     fn build(&self, app: &mut App) {
-        app.configure_set(GameSet::Input.before(GameSet::Logic))
-            .configure_set(GameSet::LogicMovement.in_set(GameSet::Logic))
-            .configure_set(GameSet::LogicAction.in_set(GameSet::Logic))
-            .configure_set(GameSet::LogicCollisions.in_set(GameSet::Logic))
+        app
+            // states
+            .configure_sets(
+                (GameSet::Input, GameSet::Logic, GameSet::Depiction)
+                    .in_set(OnUpdate(AppState::Game)),
+            )
+            // containers
+            .configure_sets(
+                (
+                    GameSet::LogicMovement,
+                    GameSet::LogicAction,
+                    GameSet::LogicCollisions,
+                )
+                    .in_set(GameSet::Logic),
+            )
+            // dependencies
+            .configure_set(GameSet::Input.before(GameSet::Logic))
             .configure_set(GameSet::LogicMovement.before(GameSet::LogicCollisions))
             .configure_set(GameSet::LogicCollisions.before(GameSet::LogicAction))
             .configure_set(GameSet::Logic.before(GameSet::Depiction));
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,41 @@ use bevy::prelude::*;
 use game::GamePlugin;
 
 fn main() {
+    println!("Menu State");
+
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_state::<AppState>()
         .add_plugin(GamePlugin)
-        .add_system(exit_game)
+        .add_systems((toggle_menu, exit_game))
         .run();
 }
 
 pub fn exit_game(input: Res<Input<KeyCode>>, mut app_exit_sender: EventWriter<AppExit>) {
     if input.just_pressed(KeyCode::Escape) {
         app_exit_sender.send(AppExit);
+    }
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum AppState {
+    #[default]
+    Menu,
+    Game,
+}
+
+pub fn toggle_menu(
+    input: Res<Input<KeyCode>>,
+    current_state: Res<State<AppState>>,
+    mut next_state: ResMut<NextState<AppState>>,
+) {
+    if input.just_pressed(KeyCode::Space) {
+        next_state.set(if current_state.0 == AppState::Menu {
+            println!("Game State");
+            AppState::Game
+        } else {
+            println!("Menu State");
+            AppState::Menu
+        });
     }
 }
